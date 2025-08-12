80e930310f2aa3f3ea8cd9396bdd5fd87ec6db7d
Isabelle Ferreira <isabellesilvamaximo@gmail.com>
2025-08-12 10:08:21 -0300

    atividade

--- atividades/atvd1.py
+++ atividades/atvd1.py
@@ -12,48 +12,45 @@ class Sistema_Clientes:
         print(f"NOME: {self.nome}")
         print(f"TELEFONE: {self.telefone}")
         print("----------------------")
-        
+
 class Adicionar_Cliente:
     def __init__(self):
         self.clientes = []
     def add_cliente(self, add_nome, add_tell):
-        self.clientes.append(add_nome, add_tell)
-        
-#add_nome = input("Insira o nome do cliente: ")
-#add_tell = input("Insira o telefone do cliente: ")
-#informaçoes = Sistema_Clientes(add_nome, add_tell)        
-#'informaçoes.exibir()
+        cliente = Sistema_Clientes(add_nome, add_tell)
+        self.clientes.append(cliente)
 
-class Lista:
-    def __init__(self):
-        pass
     def mostrar_lista(self):
         print("Lista de Clientes cadastrados")
-        for i, x in enumerate(self.clientes, star = 1):
+        if not self.clientes:
+            print("Nenhum cliente cadastrado.")
+        for i, x in enumerate(self.clientes, start=1):
             print(f"{i} - {x.nome} | {x.telefone}")
 
 # MENU
 def menu():
+    sistema = Adicionar_Cliente()
     while True:
         escolha = int(input(''' MENU DE OPÇÕES 
                     1 - Adicionar clientes
                     2 - Visualizar lista de clientes
                     3 - Sair
-                    Insira o ID do que deseja fazer: '''))
+                    Insira o indice do que deseja fazer: '''))
         if escolha == 1:
             add_nome = input("Insira o nome do cliente: ")
-            add_tell = input("Insira o telefone do cliente: ")
-            informaçoes = Sistema_Clientes(add_nome, add_tell)        
-            informaçoes.exibir()
+            add_tell = (input("Insira o telefone do cliente: "))
+            sistema.add_cliente(add_nome, add_tell)
+            print("Cliente adicionado com sucesso!")
         elif escolha == 2:
-            Lista()
+            sistema.mostrar_lista()
         elif escolha == 3:
             print("Encerrando...")
+            # salvar clientes no arquivo ao sair
+            with open("clientes.txt", "w") as arquivo:
+                for cliente in sistema.clientes:
+                    arquivo.write(f"{cliente.nome},{cliente.telefone}\n")
             break
         else:
             print("Escolha uma opção válida")
 
 menu()
-with open ("clientes.txt", "w" ) as arquivo:
-    for item in arquivo:
-                arquivo.write(str(item) + "\n")
